4253a439611e6153b55c2e46d0d49c75221f0d30
MUSA <localhost.ads@gmail.com>
2021-07-12 17:19:46 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:functions/pages/ImageConverter.dart';
+import 'package:functions/pages/youtubeTranslator.dart';
+
+
+int _currentIndex = 0;
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'functions',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.cyan,
+        accentColor: Colors.blueGrey,
+        fontFamily: 'Gothic A1',
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
+      home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery
+        .of(context)
+        .size;
+
+    final pageController = PageController();
+
+    // int _currentIndex = 0;
+
+    // void onPageChanged(int index) {
+    //   setState(() {
+    //     _currentIndex.value = index;
+    //   });
+    // }
+
+    List<Map<String, dynamic>> tileMenu = [
+      {
+        'title' : '유튜브 코드 에디터',
+        'icon' : Icons.play_arrow_sharp,
+      },
+      {
+        'title' : '이미지 소스코드 에디터',
+        'icon' : Icons.image,
+      }
+    ];
+
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('F U N C T I O N S',
+              style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),
+          centerTitle: true,
+        ),
+        body: Row(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(14),
+              width: 300,
+              height: size.height,
+              child: ListView.builder(
+                    itemCount: tileMenu.length,
+                    itemBuilder: (_, index) {
+                      // padding: EdgeInsets.zero,
+                      // children: <Widget>[
+                      return ListTile(
+                          leading: Icon(tileMenu[index]['icon'],
+                            color: (index == _currentIndex) ? Colors.cyan : Colors
+                                .black54,),
+                          title: Text(
+                              tileMenu[index]['title'], style: TextStyle(
+                              fontWeight: (_currentIndex == index) ? FontWeight.bold : FontWeight.normal,
+                              color: (_currentIndex == index) ? Colors.cyan : Colors.black87),
+                          ),
+                          onTap: () {
+                            print(_currentIndex);
+                            setState(() {
+                              _currentIndex = index;
+                            });
+                            pageController.jumpToPage(_currentIndex);
+                          }
+                      );
+                    })
+                ),
+            SizedBox(
+              width: size.width - 300,
+              height: size.height,
+              child: Container(
+                margin: EdgeInsets.only(
+                    top: 14, bottom: 14, right: 14, left: 0),
+                decoration: BoxDecoration(
+                  color: Color(0xffe8ebed),
+                ),
+                child: PageView(
+                    controller: pageController,
+                    // onPageChanged: onPageChanged,
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      YoutubeTrans(),
+                      ImageConverter(),
+                    ]),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/youtubeTranslator.dart
@@ -0,0 +1,319 @@
+import 'package:flutter/material.dart';
+import 'package:functions/functions.dart';
+
+bool autoPlay = true;
+bool loop = true;
+
+class YoutubeTrans extends StatefulWidget {
+  const YoutubeTrans({Key? key}) : super(key: key);
+
+  @override
+  _YoutubeTransState createState() => _YoutubeTransState();
+}
+
+class _YoutubeTransState extends State<YoutubeTrans> {
+  final _formKey = GlobalKey<FormState>();
+  final _sourceCodeController = TextEditingController();
+  final _convertedController = TextEditingController();
+  final _widthValueController = TextEditingController();
+  final _heightValueController = TextEditingController();
+
+  final _dropDownValue = ['가로 길이에 맞춤', '세로 길이에 맞춤', '사용자 맞춤 설정', '변경 없음'];
+  var _selectedValue = '가로 길이에 맞춤';
+
+  final _regID = RegExp(r'(?:youtube.com/embed/)([a-zA-Z0-9_]*)');
+  var regWidth = RegExp(r'(?:width\s*=\s*)"(\d+)"');
+  var regHeight = RegExp(r'(?:height\s*=\s*)"(\d+)"');
+
+  String convertSC() {
+    String sc = _sourceCodeController.text.toLowerCase();
+    int _oriWidth = int.parse(regWidth.firstMatch(sc)!.group(1)!);
+    int _oriHeight = int.parse(regHeight.firstMatch(sc)!.group(1)!);
+    String? _youtubeID = _regID.firstMatch(sc)!.group(1);
+
+    if (autoPlay && loop) {
+      sc = sc.replaceFirst(_youtubeID!, '?version=3&autoplay=1&loop=1&playlist='+_youtubeID);
+    }
+
+    if (autoPlay && !loop) {
+      sc = sc.replaceFirst(_youtubeID!, '?version=3&autoplay=1&playlist='+_youtubeID);
+    }
+
+    if (loop && !autoPlay) {
+      sc = sc.replaceFirst(_youtubeID!, '?version=3&loop=1&playlist='+_youtubeID);
+    }
+
+    if (_selectedValue == "가로 길이에 맞춤") {
+      int _wantedHeight = resizeCal(_oriWidth, _oriHeight, int.parse(_widthValueController.text), true);
+      sc = sc.replaceAll(regWidth, 'width="${_widthValueController.text}"');
+      sc = sc.replaceAll(regHeight, 'height="$_wantedHeight"');
+    }
+    if (_selectedValue == "세로 길이에 맞춤") {
+      int _wantedWidth = resizeCal(_oriWidth, _oriHeight, int.parse(_heightValueController.text), false);
+      sc = sc.replaceAll(regHeight, 'height="${_heightValueController.text}"');
+      sc = sc.replaceAll(regWidth, 'width="$_wantedWidth"');
+    }
+    if (_selectedValue == "사용자 맞춤 설정") {
+      sc = sc.replaceAll(regWidth, 'width="${_widthValueController.text}"');
+      sc = sc.replaceAll(regHeight, 'height="${_heightValueController.text}"');
+    }
+    return sc;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+        /*
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+            }
+               */
+      },
+      child: Container(
+        color: Color(0xffe8ebed),
+        child: Form(
+          key: _formKey,
+          child: Padding(
+              padding:
+                  EdgeInsets.only(top: 28, bottom: 28, right: 30, left: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("유튜브 소스코드 에디터",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(height: 13),
+                  Container(
+                      // TODO:  Switches
+                      ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.cyan),
+                      // borderRadius: BorderRadius.all(Radius.circular(5))
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          top: 0, bottom: 6, left: 10, right: 10),
+                      child: TextFormField(
+                        controller: _sourceCodeController,
+                        decoration: InputDecoration(
+                            hintText: '이곳에 원본 소스코드를 입력해주세요.',
+                            border: InputBorder.none),
+                        style: TextStyle(fontSize: 15),
+                        minLines: 12,
+                        maxLines: 12,
+                        autofocus: true,
+                        validator: (value) {
+                          if (value!.trim().isEmpty) {
+                            return '원본 소스코드가 입력되어야합니다.';
+                          } else if (_regID.allMatches(_sourceCodeController.text).length > 1) {
+                            return '다수의 소스코드를 한번에 변환할 수 없습니다.';
+                          } else if (_sourceCodeController.text.trim().split("width").length != 2
+                              || _sourceCodeController.text.trim().split("height").length != 2
+                              || _sourceCodeController.text.trim().split("embed/").length != 2
+                              || (_sourceCodeController.text.trim().split('"').length + _sourceCodeController.text.trim().split("'").length) < 10
+                              || !(_regID.hasMatch(_sourceCodeController.text))
+                              || !(_sourceCodeController.text.contains(regWidth))
+                              || !(_sourceCodeController.text.contains(regHeight))
+                          ) {
+                            return '올바른 유튜브 소스코드가 아닙니다.';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    textBaseline: TextBaseline.ideographic,
+                    children: [
+                      // TODO: 화면 크기 작아지면 잘리는데, 이거 70% / 30% 이런 식으로 나누는거 있었는데 찾아보기
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(children: [
+                            Checkbox(
+                                value: loop,
+                                onChanged: (value) {
+                                  if (value != null) {
+                                    setState(() {
+                                      loop = value;
+                                    });
+                                  }
+                                }),
+                            SizedBox(width: 8),
+                            Text(
+                              "반복 재생",
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ]),
+                          SizedBox(width: 40),
+                          Row(
+                            children: [
+                              Checkbox(
+                                  value: autoPlay,
+                                  onChanged: (value) {
+                                    if (value != null) {
+                                      setState(() {
+                                        autoPlay = value;
+                                      });
+                                    }
+                                  }),
+                              SizedBox(width: 8),
+                              Text("자동 재생", style: TextStyle(fontSize: 15)),
+                            ],
+                          ),
+                          SizedBox(width: 80),
+                          Row(
+                            children: [
+                              // Checkbox(value: resize, onChanged: (value){
+                              //   if (value != null) {
+                              //     setState(() {
+                              //       resize = value;
+                              //     });
+                              //   }
+                              // }),
+                              // SizedBox(width: 8),
+                              Text(
+                                "동영상 사이즈 변경 : ",
+                                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black54),
+                              ),
+                              SizedBox(width: 12),
+                                DropdownButton(
+                                    value: _selectedValue,
+                                    items: _dropDownValue.map((value) {
+                                      return DropdownMenuItem(
+                                        value: value,
+                                        child: Padding(
+                                          padding: EdgeInsets.only(bottom:2.8),
+                                            child: Text(value, style: TextStyle(color: Colors.black87, fontSize: 16.2))),
+                                      );
+                                    }).toList(),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _selectedValue = value.toString();
+                                      });
+                                    }),
+                              SizedBox(width: 25),
+                              if (_selectedValue == "가로 길이에 맞춤")
+                                whTextField(_widthValueController, "가    로"),
+
+                              if (_selectedValue == "세로 길이에 맞춤")
+                                whTextField(_heightValueController, "세    로"),
+
+                              if (_selectedValue == "사용자 맞춤 설정")
+                                Row(children: [
+                                  whTextField(_widthValueController, "가    로"),
+                                  SizedBox(width: 14),
+                                  Text("x"),
+                                  SizedBox(width: 14),
+                                  whTextField(_heightValueController, "세    로"),
+                                ])
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: 120,
+                        height: 40,
+                        child: ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.cyan)),
+                            onPressed: () {
+                              _formKey.currentState!.validate();
+
+                              // TODO: 변환 기능 넣어야함
+                              if (_sourceCodeController.text.isEmpty == true) {
+                                alertBox(context, "변환 실패", "원본 소스코드가 입력되어야합니다.");
+                              } else if (_regID.allMatches(_sourceCodeController.text).length > 1) {
+                                alertBox(context, "변환 실패", "다수의 소스코드를 한번에 변환할 수 없습니다.");
+                              } else if (_sourceCodeController.text.trim().split("width").length != 2
+                                  || _sourceCodeController.text.trim().split("height").length != 2
+                                  || _sourceCodeController.text.trim().split("embed/").length != 2
+                                  || (_sourceCodeController.text.trim().split('"').length + _sourceCodeController.text.trim().split("'").length) < 10
+                                  || !(_regID.hasMatch(_sourceCodeController.text))
+                                  || !(_sourceCodeController.text.contains(regWidth))
+                                  || !(_sourceCodeController.text.contains(regHeight))) {
+                                alertBox(context, "변환 실패", "올바른 유튜브 소스코드가 아닙니다.");
+                              } else if (_selectedValue == "가로 길이에 맞춤" && _widthValueController.text.isEmpty) {
+                                alertBox(context, "변환 실패", "변환할 영상의 가로 길이가 비어있습니다.");
+                              } else if (_selectedValue == "세로 길이에 맞춤" && _heightValueController.text.isEmpty) {
+                                alertBox(context, "변환 실패", "변환할 영상의 세로 길이가 비어있습니다.");
+                              } else if (_selectedValue == "사용자 맞춤 설정" && (_widthValueController.text.isEmpty || _heightValueController.text.isEmpty)) {
+                                alertBox(context, "변환 실패", "변환할 영상의 가로 혹은 세로 길이가 비어있습니다.");
+                              } else {
+                                // TODO: 변환 기능 넣어야함
+                                _convertedController.text = convertSC();
+                              }
+                            },
+                            child: Text(
+                              "변 환",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            )),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 60),
+                  // TODO: 변환 이후의 코드
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black12,
+                      border: Border.all(color: Colors.grey),
+                      // borderRadius: BorderRadius.all(Radius.circular(5))
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          top: 0, bottom: 6, left: 10, right: 10),
+                      child: TextFormField(
+                        controller: _convertedController,
+                        decoration: InputDecoration(
+                            hintText: '변환된 소스코드가 입력될 공간입니다.',
+                            border: InputBorder.none),
+                        style: TextStyle(fontSize: 15, color: Colors.black38),
+                        minLines: 12,
+                        maxLines: 12,
+                        autofocus: false,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.cyan)),
+                          onPressed: () {
+                            copyToClipboard(_convertedController);
+                            _convertedController.text.isEmpty
+                            ? alertBox(context, "복사 실패: 텍스트 상자가 비어있음", "소스코드 복사에 실패했습니다.")
+                            : alertBox(context, "복사 완료", "소스코드 복사가 정상적으로 완료되었습니다.");
+                          },
+                          child: Text("소스코드 자동 복사",
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 18))),
+                    ),
+                  ]),
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
