0d7e42523ed7b91a2cf620faf76f4829651bb60b
WebBuilder20 <67769922+WebBuilder20@users.noreply.github.com>
2022-08-26 16:37:26 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:functions/pages/ImageConverter.dart';
-import 'package:functions/pages/youtubeTranslator.dart';
+import 'package:functions/src/image_converter/image_converter.dart';
+import 'package:functions/src/youtube_translator/youtube_translator.dart';
 
 // flutter run -d chrome
 // flutter build web --release
@@ -26,11 +26,11 @@ void main() {
         brightness: Brightness.light,
         primaryColor: Colors.cyan,
         accentColor: Colors.blueGrey,
-        fontFamily: 'Gothic A1',
+        fontFamily: 'IBMPlexSansKR',
         textTheme: TextTheme(
           headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
           headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          bodyText2: TextStyle(fontSize: 14.0),
         ),
       ),
       home: MyApp()));
@@ -52,14 +52,6 @@ class _MyAppState extends State<MyApp> {
 
     final pageController = PageController();
 
-    // int _currentIndex = 0;
-
-    // void onPageChanged(int index) {
-    //   setState(() {
-    //     _currentIndex.value = index;
-    //   });
-    // }
-
     List<Map<String, dynamic>> tileMenu = [
       {
         'title' : '유튜브 코드 에디터',
@@ -77,45 +69,43 @@ class _MyAppState extends State<MyApp> {
       },
       child: Scaffold(
         appBar: AppBar(
-          title: Text('F U N C T I O N S',
-              style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),
+          title: const Text('FUNCTIONS',
+              style: TextStyle(fontFamily: 'IBMPlexSansKR', color: Colors.white, fontSize: 20, letterSpacing: 4.0, fontWeight: FontWeight.bold, fontStyle: FontStyle.normal)),
           centerTitle: true,
         ),
         body: Row(
           children: <Widget>[
             Container(
-              padding: EdgeInsets.all(14),
-              width: 300,
-              height: size.height,
-              child: ListView.builder(
+              width: size.width < 1000 ? 68 : 280,
+                child: ListView.builder(
+                  // padding: EdgeInsets.symmetric(vertical: 15),
                     itemCount: tileMenu.length,
                     itemBuilder: (_, index) {
-                      // padding: EdgeInsets.zero,
-                      // children: <Widget>[
                       return ListTile(
-                          leading: Icon(tileMenu[index]['icon'],
-                            color: (index == _currentIndex) ? Colors.cyan : Colors
-                                .black54,),
-                          title: Text(
+                        tileColor: (index == _currentIndex) ? Colors.black.withOpacity(0.1) : Colors.transparent,
+                          contentPadding: EdgeInsets.symmetric(horizontal: size.width < 1000 ? 18 : 20, vertical: 8),
+                            leading: Icon(tileMenu[index]['icon'], size: 35,
+                                color: (index == _currentIndex) ? Colors.cyan : Colors
+                                    .black38,),
+                            /// 화면 작아지면 타이틀 날리기
+                            title: (size.width < 1000) ? null : Text(
                               tileMenu[index]['title'], style: TextStyle(
-                              fontWeight: (_currentIndex == index) ? FontWeight.bold : FontWeight.normal,
-                              color: (_currentIndex == index) ? Colors.cyan : Colors.black87),
-                          ),
-                          onTap: () {
-                            setState(() {
-                              _currentIndex = index;
-                            });
-                            pageController.jumpToPage(_currentIndex);
-                          }
+                                fontWeight: (_currentIndex == index) ? FontWeight.bold : FontWeight.bold,
+                                color: (_currentIndex == index) ? Colors.cyan : Colors.black38),
+                            ),
+                            onTap: () {
+                              setState(() {
+                                _currentIndex = index;
+                              });
+                              pageController.jumpToPage(_currentIndex);
+                            }
                       );
                     })
-                ),
-            SizedBox(
-              width: size.width - 300,
-              height: size.height,
-              child: Container(
-                margin: EdgeInsets.only(
-                    top: 14, bottom: 14, right: 14, left: 0),
+            ),
+            Container(
+              width: size.width < 1000 ? size.width - 68 : size.width - 280,
+                // margin: const EdgeInsets.only(
+                //     top: 14, bottom: 14, right: 14, left: 0),
                 decoration: BoxDecoration(
                   color: Color(0xffe8ebed),
                 ),
@@ -124,11 +114,10 @@ class _MyAppState extends State<MyApp> {
                     // onPageChanged: onPageChanged,
                     physics: NeverScrollableScrollPhysics(),
                     children: [
-                      YoutubeTrans(),
-                      ImageConverter(),
+                      YoutubeTrans(size),
+                      ImageConverter(size),
                     ]),
               ),
-            ),
           ],
         ),
       ),
